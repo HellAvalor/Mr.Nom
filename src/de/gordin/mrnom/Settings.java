a06b92bc172038e7f08cfa07f012c7e0656e74b2
Gordin <9ordin@gmail.com>
2011-12-04 18:29:29 +0100

    addScore in Settings

--- src/de/gordin/mrnom/Settings.java
+++ src/de/gordin/mrnom/Settings.java
@@ -53,4 +53,15 @@ public class Settings {
         }
     }
 
+    public static void addScore(int score) {
+        for (int i = 0; i < numHighscores; i++) {
+            if (highscores[i] < score) {
+                for (int j = 4; j > i; j--)
+                    highscores[j] = highscores[j - 1];
+                highscores[i] = score;
+                break;
+            }
+        }
+    }
+
 }
